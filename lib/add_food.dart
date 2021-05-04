efe9aeb83cc76ec741c9dc5afcaada354e6cf0d9
Meysam Bavi <meybsky@gmail.com>
2021-05-04 16:28:16 +0430

    Refactor and indent add_food

--- lib/add_food.dart
+++ lib/add_food.dart
@@ -18,90 +18,106 @@ class _AddFoodState extends State<AddFood> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            buildNameField(),
+            buildDescriptionField(),
+            buildPriceField(),
+            buildCategoryDropdown(),
+            Center(
+              child: buildModelButton('Create', CommonColors.green!, createFood),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: TextFormField(
-                  validator: (name) {
-                    if (name == null || name.isEmpty) {
-                      return Strings.get('add-food-empty-name-error');
-                    }
-                  },
-                  onSaved: (name) {
-                    _name = name!;
-                  },
-                  decoration: InputDecoration(
-                    hintText: Strings.get('add-bottom-sheet-food-name')!,
-                    icon: Icon(Icons.fastfood_sharp),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: TextFormField(
-                  keyboardType: TextInputType.multiline,
-                  maxLength: 400,
-                  maxLines: 3,
-                  onSaved: (desc) {
-                    _desc = desc;
-                  },
-                  decoration: InputDecoration(
-                    hintText: Strings.get('add-bottom-sheet-food-description')!,
-                    icon: Icon(Icons.text_snippet_outlined),
-                    hintMaxLines: 3,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: TextFormField(
-                  keyboardType: TextInputType.number,
-                  validator: (String? number) {
-                    if (number == null || number.isEmpty) {
-                      return Strings.get('add-food-empty-price-error');
-                    }
-                    var parsed = int.tryParse(number);
-                    if (parsed == null || parsed <= 0) {
-                      return Strings.get('add-food-invalid-price-error');
-                    }
-                  },
-                  onSaved: (value) {
-                    _price = int.parse(value!);
-                  },
-                  decoration: InputDecoration(
-                    hintText: Strings.get('add-bottom-sheet-food-price')!,
-                    icon: Icon(Icons.attach_money_rounded),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: DropdownButton<FoodCategory>(
-                  value: _category,
-                  items: FoodCategory.values.map((category) =>
-                    DropdownMenuItem<FoodCategory>(
-                      value: category,
-                      child: Text(Strings.get(category.toString())!),
-                    )
-                  ).toList(),
-                  onChanged: (category) {
-                      setState(() {
-                        _category = category!;
-                      });
-                    },
-                ),
-              ),
-              Center(
-                child: buildModelButton('Create', CommonColors.green!, createFood),
-              ),
-            ],
-          ),
-        ));
-      }
+  Padding buildCategoryDropdown() {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: DropdownButton<FoodCategory>(
+        value: _category,
+        items: FoodCategory.values.map((category) =>
+            DropdownMenuItem<FoodCategory>(
+              value: category,
+              child: Text(Strings.get(category.toString())!),
+            )
+        ).toList(),
+        onChanged: (category) {
+          setState(() {
+            _category = category!;
+          });
+          },
+      ),
+    );
+  }
+
+  Padding buildPriceField() {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        keyboardType: TextInputType.number,
+        validator: (String? number) {
+          if (number == null || number.isEmpty) {
+            return Strings.get('add-food-empty-price-error');
+          }
+          var parsed = int.tryParse(number);
+          if (parsed == null || parsed <= 0) {
+            return Strings.get('add-food-invalid-price-error');
+          }
+          },
+        onSaved: (value) {
+          _price = int.parse(value!);
+          },
+        decoration: InputDecoration(
+          hintText: Strings.get('add-bottom-sheet-food-price')!,
+          icon: Icon(Icons.attach_money_rounded),
+        ),
+      ),
+    );
+  }
+
+  Padding buildDescriptionField() {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        keyboardType: TextInputType.multiline,
+        maxLength: 400,
+        maxLines: 3,
+        onSaved: (desc) {
+          _desc = desc;
+          },
+        decoration: InputDecoration(
+          hintText: Strings.get('add-bottom-sheet-food-description')!,
+          icon: Icon(Icons.text_snippet_outlined),
+          hintMaxLines: 3,
+        ),
+      ),
+    );
+  }
+
+  Padding buildNameField() {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        validator: (name) {
+          if (name == null || name.isEmpty) {
+            return Strings.get('add-food-empty-name-error');
+          }
+          },
+        onSaved: (name) {
+          _name = name!;
+          },
+        decoration: InputDecoration(
+          hintText: Strings.get('add-bottom-sheet-food-name')!,
+          icon: Icon(Icons.fastfood_sharp),
+        ),
+      ),
+    );
+  }
       void createFood() {
         if (!_formKey.currentState!.validate()) {
           return;
